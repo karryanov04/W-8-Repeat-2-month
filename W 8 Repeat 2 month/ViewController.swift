a630ffc9e69a4da9d430bb2b10eb965d3bb3e7a2
Raul <krolik-0304@bk.ru>
2023-02-06 23:06:00 +0600

    Initial Commit

---
+++ W 8 Repeat 2 month/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W 8 Repeat 2 month
+//
+//  Created by Рауль on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
